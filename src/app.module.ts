ca8bfa4e5d0f807bd54670117c72f87c09f82aee
4Kirubha <jaimelannister4221@gmail.com>
2023-11-16 11:26:46 +0530

    deleted users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BorrowsModule } from './borrows/borrows.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 import { CdsModule } from './cds/cds.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,13 +12,12 @@ import { AppService } from './app.service';
       type: 'postgres',
       host: 'ec2-43-204-73-16.ap-south-1.compute.amazonaws.com',
       port: 5432,
-      username: 'ubuntu',
-      password: 'AutonomintPostgres@2023',
+      username: '',
+      password: '',
       database:'borrowing',
       autoLoadEntities:true,
       synchronize:true
     }),
-    UsersModule,
     CdsModule],
   controllers: [AppController],
   providers: [AppService]
