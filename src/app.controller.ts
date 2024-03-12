5e55e6bdb83825a5c0be6d6ec88a5e36749b47cd
4Kirubha <jaimelannister4221@gmail.com>
2024-03-12 10:16:17 +0530

    Added new ssl

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,12 +9,12 @@ export class AppController {
 
     constructor(private appService: AppService) { }
 
-    @Get('/.well-known/pki-validation/F894B5F937FBF4E7BEA92EDCFB4A9E96.txt')
+    @Get('/.well-known/pki-validation/C8CA36E0FD79F98192DE65FCE6CC1023.txt')
     async giveCertificateData(@Res() res: Response) {
         try {
-            const file = fs.readFileSync('/home/ubuntu/Backend/src/F894B5F937FBF4E7BEA92EDCFB4A9E96.txt');
+            const file = fs.readFileSync('/home/ubuntu/Backend/src/C8CA36E0FD79F98192DE65FCE6CC1023.txt');
             res.setHeader('Content-Type', 'application/octet-stream');
-            res.setHeader('Content-Disposition', 'attachment; filename=F894B5F937FBF4E7BEA92EDCFB4A9E96.txt');
+            res.setHeader('Content-Disposition', 'attachment; filename=C8CA36E0FD79F98192DE65FCE6CC1023.txt');
             res.send(file);
         } catch (error) {
             console.log("error", error);
