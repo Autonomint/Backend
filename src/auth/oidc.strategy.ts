e3f5b67e4b79d4626bdfe219a2ed61d05ecece9b
4Kirubha <jaimelannister4221@gmail.com>
2024-04-04 13:39:24 +0530

    Updated oidc for dev

--- src/auth/oidc.strategy.ts
+++ src/auth/oidc.strategy.ts
@@ -6,10 +6,19 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, Client, UserinfoResponse, TokenSet, Issuer } from 'openid-client';
 
 export const buildOpenIdClient = async () => {
-  const TrustIssuer = await Issuer.discover('https://accounts.google.com/.well-known/openid-configuration');
+  // const TrustIssuer = await Issuer.discover('https://accounts.google.com/.well-known/openid-configuration');
+  // const client = new TrustIssuer.Client({
+  //   client_id: "819680115435-j7mj0srfgkmbrdum916mu0nuhd3t8i3t.apps.googleusercontent.com", // configService.get<string>('oidc.clientID'),
+  //   client_secret: "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", // configService.get<string>('oidc.clientSecret'),
+  // });
+  // return client;
+
+  const TrustIssuer = await Issuer.discover('https://www.joseon.com/.well-known/openid-configuration');
   const client = new TrustIssuer.Client({
-    client_id: "819680115435-j7mj0srfgkmbrdum916mu0nuhd3t8i3t.apps.googleusercontent.com", // configService.get<string>('oidc.clientID'),
-    client_secret: "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", // configService.get<string>('oidc.clientSecret'),
+    // token_endpoint_auth_signing_alg: "https://www.joseon.com/_special/rest/OAuth2:token",
+    // authorization_signed_response_alg: "https://www.joseon.com/_special/rest/OAuth2:auth",
+    client_id: "oaap-ahofgp-fdoj-dwdm-idww-rhtyfi64", // configService.get<string>('oidc.clientID'),
+    client_secret: "sk_slY4qta3XQdBv8f7MWiJiQw4nMqRm", // configService.get<string>('oidc.clientSecret'),
   });
   return client;
 };
@@ -22,10 +31,21 @@ export class OidcStrategy extends PassportStrategy(Strategy, 'oidc') {
     @InjectRepository(User)
     private configService: ConfigService,
     client: Client) {
+      // super({
+      //   client: client,
+      //   params: {
+      //     redirect_uri: "http://localhost:3001/auth/callback", // configService.get<string>('oidc.callbackURL'),
+      //     scope: "openid profile email",
+      //   },
+      //   passReqToCallback: false,
+      //   usePKCE: false,
+      // });
+
       super({
         client: client,
         params: {
-          redirect_uri: "http://localhost:3001/auth/callback", // configService.get<string>('oidc.callbackURL'),
+          // response_type: "id_token",
+          redirect_uri: "https://3.7.222.91/auth/callback", // configService.get<string>('oidc.callbackURL'),
           scope: "openid profile email",
         },
         passReqToCallback: false,
@@ -35,7 +55,7 @@ export class OidcStrategy extends PassportStrategy(Strategy, 'oidc') {
     this.client = client;
   }
 
-  async validate(tokenset: TokenSet): Promise<any> {
+  async validate(tokenset: TokenSet, iss:string, aud: string): Promise<any> {
     const userinfo: UserinfoResponse = await this.client.userinfo(tokenset);
 
     try {
@@ -49,6 +69,7 @@ export class OidcStrategy extends PassportStrategy(Strategy, 'oidc') {
         userinfo,
       }
       console.log(user);
+      console.log(tokenset);
       return user;
     } catch (err) {
       throw new UnauthorizedException();
